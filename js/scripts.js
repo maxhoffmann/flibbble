d59e75c2c9bc212323f1ad58516fbd2af260d13c
Maximilian Hoffmann <max@htmldocs.de>
2012-05-09 00:41:08 +0200

    Flip Cases erweitert
    Zurückflippen bei zu wenig Grad hinzugefügt
    
    Signed-off-by: Maximilian Hoffmann <max@htmldocs.de>

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ body.ontouchmove = function (e) {
 }
 
 var pages = document.querySelectorAll('.page'),
-		startY, endY, current = 0;
+		startY, endY, deg, current = 0;
 
 body.addEventListener('touchstart', flipStart, false);
 body.addEventListener('touchmove', flipMove, false);
@@ -16,43 +16,89 @@ body.addEventListener('touchend', flipEnd, false);
 setIndex();
 
 function flipStart(e) {
+
 	startY = e.targetTouches[0].pageY;
 	dist = 0;
+	deg = 0;
 }
 
 function flipMove(e) {
+
 	e.preventDefault();
 	dist = startY-e.targetTouches[0].pageY;
 
-	if ( dist > 0 && current != pages.length && (dist*0.48) <= 180 ) { // Flip Up
-		pages[current].style.webkitTransition = "";	
-		pages[current].style.webkitTransform = "rotateX("+(dist*0.48)+"deg)";
-	}
-	if ( dist < 0 && current != 0 && ((360+dist)*0.48) >= 0 ) { // Flip Down
-		pages[(current-1)].style.webkitTransition = "";
-		pages[(current-1)].style.webkitTransform = "rotateX("+((360+dist)*0.48)+"deg)";
-		if ( current < pages.length ) {
-			pages[current].style.zIndex = pages.length-current;
+	if ( dist > 0 ) { // Flip Up
+	
+		deg = dist*0.48;
+
+		if ( current != pages.length && deg <= 180 ) {
+
+			pages[current].style.webkitTransition = "";	
+			pages[current].style.webkitTransform = "rotateX(" + deg + "deg)";
+
+			if ( current!= 0 ) {
+				pages[current].style.zIndex = +pages[current-1].style.zIndex+1;
+			}
+
 		}
+
 	}
+
+	if ( dist < 0 ) { // Flip Down
+
+		deg = (360 + dist) * 0.48;
+
+		if ( current != 0 && deg >= 0 ) {
+
+			pages[(current-1)].style.webkitTransition = "";
+			pages[(current-1)].style.webkitTransform = "rotateX(" + deg +"deg)";
+
+		}
+
+	}
+
 
 }
 
 function flipEnd(e) {
 
-	if ( dist > 0 && current != pages.length ) { // Flip Up
+	if ( deg >= 90 && dist < 0 ) { // Flip Back Up
+
+		pages[(current-1)].style.webkitTransition = "all ease-out .4s";
+		pages[(current-1)].style.webkitTransform = "rotateX("+180+"deg) translateZ(0)";
+
+
+	}
+	if ( deg >= 90 && dist > 0 ) { // Flip Up
+
 		pages[current].style.webkitTransition = "all ease-out .4s";
 		pages[current].style.webkitTransform = "rotateX("+180+"deg) translateZ(0)";
+		
 		current++;
-		if ( current != pages.length ) {
-			pages[current].style.zIndex = +pages[(current-1)].style.zIndex+1;			
-		}
+
 	}
-	if ( dist < 0 && current != 0 ) { // Flip Down
+	if ( deg < 90 && dist > 0 ) { // Flip Back Down
+
+		pages[current].style.webkitTransition = "all ease-out .4s";
+		pages[current].style.webkitTransform = "rotateX("+0+"deg)";
+
+		pages[current].style.zIndex = pages.length-current;
+
+	}
+	if ( deg < 90 && dist < 0 ) { // Flip Down
+
 		pages[(current-1)].style.webkitTransition = "all ease-out .4s";
 		pages[(current-1)].style.webkitTransform = "rotateX("+0+"deg)";
+
+		if ( current != pages.length ) {
+			pages[(current-1)].style.zIndex = +pages[current].style.zIndex+1;
+		} else {
+			pages[(current-1)].style.zIndex = 1;			
+		}
 		current--;
+
 	}
+
 }
 
 function setIndex() {
