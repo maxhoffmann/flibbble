e1749dc04a5a847e28c029c81af39c9fb05604ec
Maximilian Hoffmann <max@htmldocs.de>
2012-05-09 13:15:18 +0200

    First und Last Shot hinzugefügt
    immer noch swipe up  und dann down bug
    
    Signed-off-by: Maximilian Hoffmann <max@htmldocs.de>

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ body.ontouchmove = function (e) {
 }
 
 var pages = document.querySelectorAll('.page'),
-		startY, endY, deg, time, current = 0;
+		startY, endY, deg, time, current = 1;
 
 body.addEventListener('touchstart', flipStart, false);
 body.addEventListener('touchmove', flipMove, false);
@@ -30,16 +30,23 @@ function flipMove(e) {
 
 	if ( dist > 0 ) { // Flip Up
 	
-		deg = dist*0.48;
+		if ( current < pages.length-1 ) {
 
-		if ( current != pages.length && deg <= 180 ) {
+			deg = Math.min( dist*0.48, 180 );
 
 			pages[current].style.webkitTransition = "";	
 			pages[current].style.webkitTransform = "rotateX(" + deg + "deg)";
+			
+			pages[current].style.zIndex = +pages[current-1].style.zIndex+1;
 
-			if ( current!= 0 ) {
-				pages[current].style.zIndex = +pages[current-1].style.zIndex+1;
-			}
+		}
+
+		if ( current === pages.length-1 ) { // Transform last
+
+			deg = Math.min( dist*0.48, 60 );
+
+			pages[current].style.webkitTransition = "";	
+			pages[current].style.webkitTransform = "rotateX(" + deg + "deg)";			
 
 		}
 
@@ -47,12 +54,27 @@ function flipMove(e) {
 
 	if ( dist < 0 ) { // Flip Down
 
-		deg = (360 + dist) * 0.48;
+		if ( current != 1 ) {
 
-		if ( current != 0 && deg >= 0 ) {
+			deg = Math.max( (360 + dist) * 0.48, 0 );
 
 			pages[(current-1)].style.webkitTransition = "";
 			pages[(current-1)].style.webkitTransform = "rotateX(" + deg +"deg)";
+
+			if ( deg < 90 ) {
+				pages[(current-1)].style.zIndex = +pages[current].style.zIndex+1;
+			} else {
+				pages[(current-1)].style.zIndex = +pages[(current-2)].style.zIndex+1;			
+			}
+
+		}
+
+		if ( current === 1 ) {
+
+			deg = Math.max( dist*0.48 , -50 );
+
+				pages[0].style.webkitTransition = "";
+				pages[0].style.webkitTransform = "rotateX("+deg+"deg)";
 
 		}
 
@@ -65,13 +87,19 @@ function flipEnd(e) {
 
 	var ms = new Date().getTime()-time;
 
-	if ( deg >= 90 && dist < 0 ) { // Flip Back Up
+	if ( deg < 0 && dist < 0 && current === 1 ) { // Flip First Back Up
 
-		pages[(current-1)].style.webkitTransition = "all ease-out .6s";
-		pages[(current-1)].style.webkitTransform = "rotateX("+180+"deg) translateZ(0)";
+			pages[(current-1)].style.webkitTransition = "all ease-out .6s";
+			pages[(current-1)].style.webkitTransform = "rotateX("+0+"deg) translateZ(0)";
 
 	}
-	if ( ( deg >= 90 || ms < 300 ) && dist > 0) { // Flip Up
+	if ( deg >= 90 && dist < 0 && current != 1 ) { // Flip Back Up
+
+			pages[(current-1)].style.webkitTransition = "all ease-out .6s";
+			pages[(current-1)].style.webkitTransform = "rotateX("+180+"deg) translateZ(0)";
+
+	}
+	if ( ( deg >= 90 || ms < 300 ) && dist > 0 && current < pages.length-1 ) { // Flip Up
 
 		pages[current].style.webkitTransition = "all ease-out .4s";
 		pages[current].style.webkitTransform = "rotateX("+180+"deg) translateZ(0)";
@@ -79,7 +107,7 @@ function flipEnd(e) {
 		current++;
 
 	}
-	if ( deg < 90 && dist > 0 ) { // Flip Back Down
+	if ( deg < 90 && dist > 0 && current != pages.length ) { // Flip Back Down
 
 		pages[current].style.webkitTransition = "all ease-out .6s";
 		pages[current].style.webkitTransform = "rotateX("+0+"deg)";
@@ -87,16 +115,12 @@ function flipEnd(e) {
 		pages[current].style.zIndex = pages.length-current;
 
 	}
-	if ( ( deg < 90 || ms < 300 ) && dist < 0 ) { // Flip Down
+	if ( ( deg < 90 || ms < 300 ) && dist < 0 && current != 1 ) { // Flip Down
 
 		pages[(current-1)].style.webkitTransition = "all ease-out .4s";
 		pages[(current-1)].style.webkitTransform = "rotateX("+0+"deg)";
+		pages[(current-1)].style.zIndex = +pages[current].style.zIndex+1;
 
-		if ( current != pages.length ) {
-			pages[(current-1)].style.zIndex = +pages[current].style.zIndex+1;
-		} else {
-			pages[(current-1)].style.zIndex = 1;			
-		}
 		current--;
 
 	}
