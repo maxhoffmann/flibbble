d868e29505bced607ced659ef5280c036978024f
Maximilian Hoffmann <max@htmldocs.de>
2012-05-15 13:54:04 +0200

    added render function
    Signed-off-by: Maximilian Hoffmann <max@htmldocs.de>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+
+/* Initialization */
+
 var body = document.body,
 		flip = document.getElementsByClassName('flip')[0],
 		pages = document.querySelectorAll('.page'),
@@ -8,11 +12,107 @@ body.ontouchmove = function (e) {
 	e.preventDefault();
 }
 
-setIndex();
-
 flip.addEventListener('touchstart', flipStart, false);
 flip.addEventListener('touchmove', flipMove, false);
 flip.addEventListener('touchend', flipEnd, false);
+
+JSONP.get( 'http://api.dribbble.com/shots/popular', {per_page:'20', page:'1'}, function(data) { renderShots(data); } );
+
+/* Render */
+
+function renderShots(data) {
+	
+	var length = data.shots.length,
+	container  = document.getElementsByClassName('flip')[0],
+	i          = 0;
+
+	console.log(length);
+
+	for ( ; i < length; i++ ) {
+
+		if ( i === 0 ) {
+			container.appendChild(renderFirstPage(data.shots[0].image_url, data.shots[0].title));			
+		}
+		if ( i === length-1 ) {
+
+			if ( length%2 === 0) {
+				container.appendChild(renderLastPage(data.shots[length-1].image_url, data.shots[length-1].title));
+			} else {
+				container.appendChild(renderLastPage());
+			}			
+		}
+		if ( i !== 0 && i !== length-1 ) {
+			container.appendChild(renderPage(data.shots[i].image_url, data.shots[i].title,data.shots[i+1].image_url, data.shots[i+1].title));
+			if ( i !== length-2 ) {
+				i++;
+			}
+		}
+	}
+
+	pages = document.querySelectorAll('.page');
+	setIndex();
+
+	function renderFirstPage(url, title) {
+
+		var page        = document.createElement('div'),
+		front           = document.createElement('div'),
+		shot            = document.createElement('img');
+		page.className  = "page first";
+		front.className = "front shot";
+		shot.src        = url;
+		shot.alt        = title;
+		front.appendChild(shot);
+		page.appendChild(front);
+		return page;
+
+	}
+
+	function renderPage(url1, title1, url2, title2 ) {
+
+		var page        = document.createElement('div'),
+		front           = document.createElement('div'),
+		back						= document.createElement('div'),
+		shot1           = document.createElement('img'),
+		shot2           = document.createElement('img');	
+		page.className  = "page";
+		front.className = "front shot";
+		back.className  = "back shot";		
+		shot1.src       = url1;
+		shot1.alt       = title1;
+		shot2.src       = url2;
+		shot2.alt       = title2;
+		front.appendChild(shot1);
+		back.appendChild(shot2);
+		page.appendChild(front);
+		page.appendChild(back);
+		return page;
+
+	}
+
+	function renderLastPage(url, title) {
+		
+		var page        = document.createElement('div'),
+		front           = document.createElement('div');
+		page.className  = "page last";
+
+		if ( url && title ) {
+			var shot        = document.createElement('img');
+			front.className = "front shot";
+			shot.src        = url;
+			shot.alt        = title;
+			front.appendChild(shot);
+		} else {
+			front.className = "front text";
+			front.innerHTML = "END";
+		}
+		
+		page.appendChild(front);
+		return page;
+	}
+
+}
+
+/* Swipe Functions */
 
 function flipStart(e) {
 	startY = e.targetTouches[0].pageY;
@@ -56,7 +156,6 @@ function flipMove(e) {
 
 			pages[current].style.webkitTransition = "";	
 			pages[current].style.webkitTransform = "rotateX(" + -deg + "deg)";	
-			console.log(deg);		
 
 		}
 
@@ -105,19 +204,19 @@ function flipEnd(e) {
 
 	if ( deg < 0 && distY < 0 && current === 1 ) { // flip first back up
 
-			pages[(current-1)].style.webkitTransition = "all ease-out .6s";
-			pages[(current-1)].style.webkitTransform = "rotateX("+0+"deg) translateZ(0)";
+		pages[(current-1)].style.webkitTransition = "all ease-out .6s";
+		pages[(current-1)].style.webkitTransform = "rotateX("+0+"deg) translateZ(0)";
 
 	}
 	if ( deg >= 90 && distY < 0 && current !== 1 ) { // flip back up
 
-			pages[(current-1)].style.webkitTransition = "all ease-out .6s";
-			pages[(current-1)].style.webkitTransform = "rotateX("+180+"deg) translateZ(0)";
+		pages[(current-1)].style.webkitTransition = "all ease-out .6s";
+		pages[(current-1)].style.webkitTransform = "rotateX("+180+"deg) translateZ(0)";
 
-			// update z-Index of next page
-			if ( current < pages.length-1 ) {
-				pages[current].style.zIndex = +pages[(current+1)].style.zIndex+1;
-			}
+		// update z-Index of next page
+		if ( current < pages.length-1 ) {
+			pages[current].style.zIndex = +pages[(current+1)].style.zIndex+1;
+		}
 
 	}
 	if ( ( deg >= 90 || ms < 300 ) && distY > 0 && current < pages.length-1 ) { // flip up
@@ -149,7 +248,7 @@ function flipEnd(e) {
 
 	}
 
-	// Slide aside
+	/* Slide aside
 	if ( distX < -100 ) {
 		flip.style.webkitTransition = "-webkit-transform ease-out .2s";
 		flip.style.webkitTransform = "translateX("+280+"px)";
@@ -159,8 +258,8 @@ function flipEnd(e) {
 		flip.style.webkitTransition = "-webkit-transform ease-out .2s";
 		flip.style.webkitTransform = "translateX("+0+"px)";
 	}
-
-	console.log( distX+"x "+distY+"y" );
+	*/
+	
 	time = 0;
 
 }
@@ -173,4 +272,5 @@ function setIndex() {
 	}
 }
 
+});
 
